ded813b9b11250f28a804100fbed8ef55e4d6dc0
VuKieuAnh <vukieuanh.hnue@gmail.com>
2024-03-14 11:55:01 +0700

    add security

---
+++ src/main/java/com/example/demo/DemoC10SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoC10SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoC10SpringBootApplication.class, args);
+	}
+
+}
